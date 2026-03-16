20c06f0bbd57e4ee54091971be3c047e5d2c3c00
swe-arena <swe-arena@local>
2026-03-16 10:07:38 +0000

    vote-2

--- index.js
+++ index.js
@@ -2,15 +2,51 @@
  * Goa Framework
  * A minimal, expressive HTTP middleware framework for Node.js
  * Inspired by Koa
+ *
+ * Modular Architecture:
+ * - lib/shared/     : Shared utilities and constants
+ * - lib/compose/    : Middleware composition
+ * - lib/request/    : HTTP request abstraction
+ * - lib/response/   : HTTP response abstraction
+ * - lib/context/    : Request/response context
+ * - lib/application/: Application core
  */
 
-import Application from './lib/application.js';
-import Context from './lib/context.js';
-import Request from './lib/request.js';
-import Response from './lib/response.js';
-import { compose } from './lib/compose.js';
+// Core Classes
+import Application from './lib/application/index.js';
+import Context from './lib/context/index.js';
+import Request from './lib/request/index.js';
+import Response from './lib/response/index.js';
 
-// Export Application as default
+// Compose function
+import { compose } from './lib/compose/index.js';
+
+// Shared utilities and constants
+export * from './lib/shared/utils.js';
+export * from './lib/shared/constants.js';
+
+// Module-specific exports
+export * from './lib/compose/index.js';
+export * from './lib/compose/utils.js';
+export * from './lib/compose/constants.js';
+
+export * from './lib/request/index.js';
+export * from './lib/request/utils.js';
+export * from './lib/request/constants.js';
+
+export * from './lib/response/index.js';
+export * from './lib/response/utils.js';
+export * from './lib/response/constants.js';
+
+export * from './lib/context/index.js';
+export * from './lib/context/utils.js';
+export * from './lib/context/constants.js';
+
+export * from './lib/application/index.js';
+export * from './lib/application/utils.js';
+export * from './lib/application/constants.js';
+
+// Export Application as default (main entry point)
 export default Application;
 
 // Named exports for individual components
